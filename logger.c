31108a77770e4088256a21e977f0588851398486
VVhatIsLove3 <153982647+VVhatIsLove3@users.noreply.github.com>
2025-03-07 13:47:36 +0300

    4 comm

--- logger.c
+++ logger.c
@@ -1,24 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-static FILE *log_file = NULL;
-
 void set_log_path(const char *path) {
-    if (log_file) {
-        fclose(log_file);
-    }
-    log_file = fopen(path, "w");
-    if (!log_file) {
-        perror("fopen");
+    if (freopen(path, "w", stdout) == NULL) {
+        perror("Не удалось перенаправить стандартный вывод");
         exit(EXIT_FAILURE);
     }
 }
-
-void log_message(const char *message) {
-    if (log_file) {
-        fprintf(log_file, "%s\n", message);
-    } else {
-        printf("%s\n", message);
-    }
-}
-
