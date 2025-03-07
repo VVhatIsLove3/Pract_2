3dac93d36a89b9b2e326ef371b17337b15648204
VVhatIsLove3 <153982647+VVhatIsLove3@users.noreply.github.com>
2025-03-07 13:47:55 +0300

    6 comm

--- logger.h
+++ logger.h
@@ -1,11 +1,7 @@
-#ifndef LOGGER_H
-#define LOGGER_H
+#ifndef LOG_H
+#define LOG_H
 
 // Установка пути для лог-файла
 void set_log_path(const char *path);
 
-// Запись сообщения в лог-файл или на экран
-void log_message(const char *message);
-
 #endif // LOGGER_H
-
